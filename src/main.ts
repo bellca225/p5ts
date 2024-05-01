cb50284ee1853fac5d0d6126a131f129a10fde60
bellca225 <nywoo0225@gmail.com>
2024-05-01 13:20:27 +0900

    ts eslint, prettier setting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import p5 from "p5";
-
 import "./style.css";
 
 const _app = new p5((p5Instance) => {
   const p = p5Instance as unknown as p5;
 
-  const x = 120;
+  const x = 100;
   const y = 100;
 
   p.setup = function setup() {
